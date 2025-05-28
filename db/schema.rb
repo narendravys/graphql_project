4fcea4c05a7b73b14ade5a6b858f9760ed8b1c0c
narendra <narendravyas246@gmail.com>
2025-05-28 21:41:54 +0000

    updated database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_05_28_205108) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -20,7 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_28_205108) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.integer "author_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_books_on_author_id"
